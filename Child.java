fbfaf682fb6cfcc2f82cab72329af3dba068e57e
Praveen <praveensingam4@gmail.com>
2023-09-01 12:17:38 +0530

    checking restore command

--- Child.java
+++ Child.java
@@ -1,5 +1,5 @@
 public Child{
  public static void main(Strings[] str){
-   System.out.println("Welcome to child class of mvn stand alone parent application..");
+   System.out.println("Welcome to child class..");
  }
 }
