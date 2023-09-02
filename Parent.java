13fb8f01b455205b4902752e50809f12c175be7c
Praveen <praveensingam4@gmail.com>
2023-09-02 09:30:34 +0530

    modified parent class

--- Parent.java
+++ Parent.java
@@ -1,5 +1,9 @@
-public Class{
+ class Parent{
  public static void main(String[] args){
   System.out.println("Hello world");
+  property();
  }
+  public static void property(){
+    System.out.println("I have some properties");
+  }
 }
